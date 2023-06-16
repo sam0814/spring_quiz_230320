e973bcddeda47c048c0bbd51165df20ff21338da
songsaemi <songsaemiye@naver.com>
2023-06-16 18:40:41 +0900

    230616 3강 quiz

---
+++ src/main/java/com/quiz/lesson03/Lesson03Quiz01RestController.java
@@ -0,0 +1,33 @@
+package com.quiz.lesson03;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.lesson03.bo.RealEstateBO;
+import com.quiz.lesson03.domain.RealEstate;
+
+@RequestMapping("/lesson03/quiz01")
+@RestController
+public class Lesson03Quiz01RestController {
+	
+	@Autowired
+	private RealEstateBO realEstateBO;
+
+	@RequestMapping("/1")
+	public RealEstate quiz01_1(
+			@RequestParam("id") int id) {
+		
+		return realEstateBO.getRealEstateById(id);
+	}
+	
+	@RequestMapping("/2")
+	public List<RealEstate> quiz01_2(
+			@RequestParam(value = "rent_price") int rentPrice) {
+		
+		return realEstateBO.getRealEstateListByRentPrice(rentPrice);
+	}
+}
